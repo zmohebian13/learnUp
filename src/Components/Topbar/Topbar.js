373c0a7dfc4084ffb7630c2092a67a8887ec9079
Zohre Mohebian <107933502+zmohebian13@users.noreply.github.com>
2023-08-15 03:06:36 +0330

    'develope-Topbar-component'

--- src/Components/Topbar/Topbar.js
+++ src/Components/Topbar/Topbar.js
@@ -1,11 +1,60 @@
-import React from 'react'
+import React from "react";
 
-import './Topbar.css'
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+import "./Topbar.css";
 
 export default function Topbar() {
   return (
-    <div>
-        
+    <div className="Topbar">
+      <Navbar expand="lg" className="bg-body-tertiary" fixed="top">
+        <Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Navbar.Brand href="#home">
+                <img
+                  src="./assests/images/logo.png"
+                  alt=""
+                  className="logo-img"
+                />
+              </Navbar.Brand>
+              <Nav.Link href="#home">خانه</Nav.Link>
+              <NavDropdown title="دوره های آموزشی" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="#home">بلاگ</Nav.Link>
+              <Nav.Link href="#home">فروشگاه</Nav.Link>
+              <Nav.Link href="#home">تماس با ما</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+          <div className="topbar-login-section">
+            <button className="login-btn">
+              <a href="#" className="topbar-login-link">
+                ورود
+              </a>
+            </button>
+            <button className="register-btn">
+              <a href="#" className="topbar-register-link">
+                عضویت
+              </a>
+            </button>
+          </div>
+        </Container>
+      </Navbar>
     </div>
-  )
+  );
 }
--- src/Pages/Index/Index.js
+++ src/Pages/Index/Index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
 import './Index.css'
+import Topbar from '../../Components/Topbar/Topbar'
 
 export default function Index() {
   return (
     <div>
-      Index
+      <Topbar />
     </div>
   )
 }
