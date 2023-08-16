547eb8101f8fa1bb2ac54ab438b6d4410ff951ea
Zohre Mohebian <107933502+zmohebian13@users.noreply.github.com>
2023-08-16 14:44:34 +0330

    'bulid-suggested-course-component'

---
+++ src/Components/SuggestedCourse/SuggestedCourse.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import './SuggestedCourse.css'
+
+export default function SuggestedCourse() {
+  return (
+    <div className="suggested-course">
+      <div className="suggested-title">
+        <p>دسته بندی های منتخب</p>
+        <h3>دوره های پیشنهادی جشنواره</h3>
+      </div>
+      <div className="container">
+        <div className="row">
+            <div className="col-lg-4 col-md-6 col-sm-12">
+                
+            </div>
+            <div className="col-lg-4 col-md-6 col-sm-12"></div>
+            <div className="col-lg-4 col-md-6 col-sm-12"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/Pages/Index/Index.js
+++ src/Pages/Index/Index.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 import './Index.css'
 import Header from '../../Components/Header/Header'
+import SuggestedCourse from '../../Components/SuggestedCourse/SuggestedCourse'
 
 export default function Index() {
   return (
     <div>
       <Header />
+      <SuggestedCourse />
     </div>
   )
 }
